a769d25448d7ea09a59532bf099ba2f569c59f9b
python-automation-aws <79166327+python-automation-aws@users.noreply.github.com>
2021-02-16 11:13:00 -0800

    Update SNS_Publish_MSG_PROD.py

--- SNS_Publish_MSG_PROD.py
+++ SNS_Publish_MSG_PROD.py
@@ -1,12 +1,14 @@
 def sns_publish():
     import boto3
     import os,datetime
-
+    # Once you execute this script it will ask your username which you update in your credential file. 
     aws_mg_con=boto3.session.Session(profile_name=input("Please enter your AWS Prod environment Username"))
+    # service name is 'sns' and region is 'us-west-2'
     sns_client=aws_mg_con.client(service_name='sns',region_name='us-west-2')
     sns_r=aws_mg_con.resource(service_name='sns',region_name='us-west-2')
     #sns_client=boto3.client(service_name='sns',region_name='us-west-2')
     #sns_resources=boto3.resource(service_name='sns',region_name='us-west-2')
+    # joining path with current date  and time folder
     path_join=os.path.join(r'C:\download\python\script',f"{datetime.datetime.now().strftime('%Y-%m-%d')}")
 
     directory_path=os.listdir(path_join)
